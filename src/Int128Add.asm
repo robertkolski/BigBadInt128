b00445ff1396210355940bbf4ed745706cd91e6f
RobertKolski <robert.kolski@outlook.com>
2018-06-13 20:14:08 -0700

    Add is a leaf function

--- src/Int128Add.asm
+++ src/Int128Add.asm
@@ -3,39 +3,53 @@ _text SEGMENT
 public Int128Add
 
 ; Int128Add
-; ---------
-; RCX - QWORD - PTR to Int128 (input1)
-; RDX - QWORD - PTR to Int128 (input2)
-; R8  - QWORD - PTR to Int128 (result)
+; -------------------------------------
+; Signed and unsigned add.
+; RCX - PTR to OWORD (input1)
+; RDX - PTR to OWORD (input2)
+; R8  - PTR to OWORD (result)
 ; R9  - unused
-; ---------
-; RAX volatile
+; -------------------------------------
+; RAX volatile - but not used
 ; R10 volatile
 ; R11 volatile
-;----------
-; C Header
-; void Int128Add(_m128* const input1, _m128* const input2, _m128* result )
-;----------
-; source remains unchanged
-; performs *R8 = *RCX + *RDX in 128 bit mode
-Int128Add PROC FRAME  
-   push rbp  
-.pushreg rbp  
-   sub rsp, 010h  
-.allocstack 010h  
-   mov rbp, rsp  
-.setframe rbp, 0  
-.endprolog  
-   mov rax, QWORD PTR [rcx]
-   mov r10, QWORD PTR [rcx+8]
-   add rax, QWORD PTR [rdx]
-   adc r10, QWORD PTR [rdx+8]
-   mov QWORD PTR [r8], rax
-   mov QWORD PTR [r8+8], r10
-
-   ; epilog  
-   add rsp, 010h  
-   pop rbp  
+;--------------------------------------
+; C Header - pseudocode prototype
+; void Int128Add(_int128* input1, _int128* input2, _int128* result )
+; assume that I have a C compiler that supports _int128 or it is a struct
+;--------------------------------------
+; C# types (without full implementation here):
+; public struct Int128
+; {
+;    private Int64 loQWORD;
+;    private Int64 hiQWORD;
+;    [DllImport("BigBadInt128.dll")]
+;    private static extern void Int128Add(IntPtr addend1, IntPtr addend2, IntPtr result);
+;    // public methods not shown
+; }
+; public struct UInt128
+; {
+;    private UInt64 loQWORD;
+;    private UInt64 hiQWORD; 
+;    [DllImport("BigBadInt128.dll")]
+;    private static extern void Int128Add(IntPtr addend1, IntPtr addend2, IntPtr result);
+;    // public methods not shown
+; }
+;--------------------------------------
+; input1 and input2 remain unchanged
+; the contents of the OWORD result is modified
+;--------------------------------------
+; Don't need FRAME and PROLOG because
+; this is a leaf function
+; it does not need any stack space
+; -------------------------------------
+Int128Add PROC
+   mov r10, QWORD PTR [rcx]
+   mov r11, QWORD PTR [rcx+8]
+   add r10, QWORD PTR [rdx]
+   adc r11, QWORD PTR [rdx+8]
+   mov QWORD PTR [r8], r10
+   mov QWORD PTR [r8+8], r11
    ret  
 Int128Add ENDP  
 _text ENDS  
