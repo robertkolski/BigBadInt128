54952f461529f519f671d798e0cd090a5e3386f3
RobertKolski <robert.kolski@outlook.com>
2018-06-15 00:16:16 -0700

    Int128Add return Int128

--- src/Int128Add.asm
+++ src/Int128Add.asm
@@ -2,21 +2,22 @@ _text SEGMENT
 
 public Int128Add
 
+
 ; Int128Add
 ; -------------------------------------
 ; Signed and unsigned add.
-; RCX - PTR to OWORD (input1)
-; RDX - PTR to OWORD (input2)
-; R8  - PTR to OWORD (result)
+; RCX - PTR to OWORD (return value buffer)
+; RDX - PTR to OWORD (input1)
+; R8  - PTR to OWORD (input2)
 ; R9  - unused
 ; -------------------------------------
-; RAX volatile - but not used
+; RAX - returns pointer to buffer
 ; R10 volatile
 ; R11 volatile
 ;--------------------------------------
 ; C Header - pseudocode prototype
-; void Int128Add(_int128* input1, _int128* input2, _int128* result )
-; assume that I have a C compiler that supports _int128 or it is a struct
+; _int128 Int128Add(_int128* input1, _int128* input2)
+; assume that I have a Visual Studio C compiler that supports _int128 or it is a struct
 ;--------------------------------------
 ; C# types (without full implementation here):
 ; public struct Int128
@@ -24,7 +25,7 @@ public Int128Add
 ;    private Int64 loQWORD;
 ;    private Int64 hiQWORD;
 ;    [DllImport("BigBadInt128.dll")]
-;    private static extern void Int128Add(IntPtr addend1, IntPtr addend2, IntPtr result);
+;    private static extern Int128 Int128Add(ref Int128 addend1, ref Int128 addend2);
 ;    // public methods not shown
 ; }
 ; public struct UInt128
@@ -32,7 +33,7 @@ public Int128Add
 ;    private UInt64 loQWORD;
 ;    private UInt64 hiQWORD; 
 ;    [DllImport("BigBadInt128.dll")]
-;    private static extern void Int128Add(IntPtr addend1, IntPtr addend2, IntPtr result);
+;    private static extern Int128 Int128Add(ref Int128 addend1, ref Int128 addend2);
 ;    // public methods not shown
 ; }
 ;--------------------------------------
@@ -44,13 +45,15 @@ public Int128Add
 ; it does not need any stack space
 ; -------------------------------------
 Int128Add PROC
-   mov r10, QWORD PTR [rcx]
-   mov r11, QWORD PTR [rcx+8]
-   add r10, QWORD PTR [rdx]
-   adc r11, QWORD PTR [rdx+8]
-   mov QWORD PTR [r8], r10
-   mov QWORD PTR [r8+8], r11
+   mov r10, QWORD PTR [rdx]
+   mov r11, QWORD PTR [rdx+8]
+   add r10, QWORD PTR [r8]
+   adc r11, QWORD PTR [r8+8]
+   mov QWORD PTR [rcx], r10
+   mov QWORD PTR [rcx+8], r11
+   mov rax, rcx
    ret  
 Int128Add ENDP  
+
 _text ENDS  
 END
